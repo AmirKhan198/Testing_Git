535b262a47128902cbbcbe0981d00f8037955a3f
Amirkhan198 <amirk198786@gmail.com>
2026-09-25 13:11:49 +0530

    tt

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
     }
     public Action GetAction()
     {
+        Console.WriteLine("Version D2");
         Console.WriteLine("Version P42");
         return () => Console.WriteLine("Version V4");
     }
